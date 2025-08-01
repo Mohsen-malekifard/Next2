a9d2e0287c967e4fe68a8a4760168b3589672056
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-01 19:19:56 +0000

    a

--- my-app/src/app/Blog/page.jsx
+++ my-app/src/app/Blog/page.jsx
@@ -1,10 +1,11 @@
 import {data} from "@/app/lib/data";
 
 export default function Blog() {
+    const blogs = data.map(a => {
+        <h1>{a.title}</h1>
+    })
     return (
-        data.map(a => {
-            <h1>{a.title}</h1>
-        })
+        {blogs}
     )
 }
 
