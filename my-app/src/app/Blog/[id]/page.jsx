1cd74fe187fc46f606b4f2dd9fd766312862f7bc
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 07:32:26 +0000

    t

---
+++ my-app/src/app/Blog/[id]/page.jsx
@@ -0,0 +1,12 @@
+import data5 from "@/app/lib/data";
+import Link from "next/link";
+
+export default function getDetails({params.id}) {
+    const found = data5.find((blog) => blog.id == params.id);
+    return (
+        <>
+            <h1>{found.title}</h1>
+            <h1>{found.context}</h1>
+        </>
+    )
+}
