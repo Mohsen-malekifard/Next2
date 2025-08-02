fdcf035ff0d4f6f43e84de4ba1be5c2baa2adc30
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 06:15:32 +0000

    e

--- my-app/src/app/Home.jsx
+++ my-app/src/app/Home.jsx
@@ -1,7 +1,10 @@
+import BlogCards from "@/components/BlogCards";
+
 export default function HomePage() {
     return (
         <>
             <h1>Welcome to my blog site</h1>
+            <BlogCards/>
         </>
     )
 }
