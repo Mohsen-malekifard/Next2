1a82cf0eac178bbecd91ea9ed7879274ac934153
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 10:11:55 +0000

    m

--- my-app/src/app/About/page.jsx
+++ my-app/src/app/About/page.jsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 
 export default function AboutPage() {
     return (
-        <>
+        <div style = {{"backgroundColor" : "yellow", "padding" : "10px", "borderRadius" : "10px"}}>
             <h1>About Page</h1>
             <p>FullName : Mohsen Malekifard</p>
             <p>Email : mohsenmaleki1389@gmail.com</p>
             <p>GitHub Address: github.com/Mohsen-Malekifard</p>
-            <p>Updated</p>
 
-        </>
+        </div>
     )
 }
