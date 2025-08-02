b1b0adbd84f9021261b112c86a526a12c34bdfd1
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 13:31:00 +0000

    U

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 export default function NavBar() {
     return (
         <ul class="flex justify-center space-x-6 bg-red-600 py-4">
-            <li><a href="#" class="text-white text-lg font-semibold hover:text-gray-300">Home Page</a></li>
-            <li><a href="#" class="text-white text-lg font-semibold hover:text-gray-300">Blog List</a></li>
-            <li><a href="#" class="text-white text-lg font-semibold hover:text-gray-300">About Page</a></li>
+            <li><a href="/" class="text-white text-lg font-semibold hover:text-gray-300">Home Page</a></li>
+            <li><a href="/Blog" class="text-white text-lg font-semibold hover:text-gray-300">Blog List</a></li>
+            <li><a href="/About" class="text-white text-lg font-semibold hover:text-gray-300">About Page</a></li>
         </ul>
 
     )
