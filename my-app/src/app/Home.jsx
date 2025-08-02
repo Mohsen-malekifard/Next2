0a9b23348774a1efeccefda170f971fd00a0d7fd
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 19:51:56 +0000

    Add Styles For Button

--- my-app/src/app/Home.jsx
+++ my-app/src/app/Home.jsx
@@ -1,4 +1,5 @@
 import BlogCard from "@/components/BlogCard";
+import Link from "next/link";
 
 export default function HomePage() {
 
@@ -6,6 +7,7 @@ export default function HomePage() {
         <>
             <h1 style = {{"fontSize" : "50px"}}>Welcome to my blog site</h1>
             <BlogCard/>
+            <Link href = "Blog" className="mt-5 bg-yellow-400 px-4 py-2 mx-auto block text-center rounded">Show more</Link>
 
 
         </>
