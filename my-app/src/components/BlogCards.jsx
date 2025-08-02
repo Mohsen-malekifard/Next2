86d27e968a4ef5ce8c665f5d68825cb588216543
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 14:44:11 +0000

    MN

--- my-app/src/components/BlogCards.jsx
+++ my-app/src/components/BlogCards.jsx
@@ -7,12 +7,18 @@ export default function BlogCards() {
 
 
     return (
-        <ul>
+        <ul className="space-y-4">
             {data5.map((blog, index) => (
-                <li key = {index} >
-                    <Link style = {{"backgroundColor" : "white", "color" : "black"}} href = {`Blog/${blog.title}`}>{blog.title}</Link>
+                <li key={index} className="rounded-lg shadow-lg overflow-hidden">
+                <Link 
+                    className="block px-4 py-2 text-lg font-semibold bg-white text-black hover:bg-gray-200 transition-colors duration-200 rounded-md"
+                    href={`Blog/${blog.title}`}
+                >
+                {blog.title}
+                </Link>
                 </li>
             ))}
         </ul>
+
     );
 }
--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 export default function NavBar() {
     return (
-        <ul class="flex justify-center space-x-6 bg-red-600 py-4">
-            <li><a href="/" class="text-white text-lg font-semibold hover:text-gray-300">Home Page</a></li>
-            <li><a href="/Blog" class="text-white text-lg font-semibold hover:text-gray-300">Blog List</a></li>
-            <li><a href="/About" class="text-white text-lg font-semibold hover:text-gray-300">About Page</a></li>
+        <ul className="flex justify-center space-x-6 bg-red-600 py-4">
+            <li><a href="/" className="text-white text-lg font-semibold hover:text-gray-300">Home Page</a></li>
+            <li><a href="/Blog" className="text-white text-lg font-semibold hover:text-gray-300">Blog List</a></li>
+            <li><a href="/About" className="text-white text-lg font-semibold hover:text-gray-300">About Page</a></li>
         </ul>
 
     )
