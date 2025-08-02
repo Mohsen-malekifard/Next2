cfffdc5ce5bb96217c4b1cd20fff355e27694fae
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 07:55:03 +0000

    b

---
+++ my-app/src/app/About/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>FullName : Mohsen Malekifard</p>
+            <p>Email : mohsenmaleki1389@gmail.com</p>
+            <p>GitHub Address: github.com/Mohsen-Malekifard</p>
+
+        </>
+    )
+}
