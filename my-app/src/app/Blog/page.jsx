27dab3553a43d6f564f3561b7a97e9f517105ee6
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 14:32:07 +0000

    M

--- my-app/src/app/Blog/page.jsx
+++ my-app/src/app/Blog/page.jsx
@@ -1,17 +1,12 @@
 import data5 from "@/app/lib/data"; // یا: { data5 } اگر named export باشه
+import BlogCards from "@/components/BlogCards";
 import Link from "next/link";
 
 export default function Blog() {
   return (
     <>
       <h1 style = {{"fontSize" : "50px"}}>Blog Page</h1>
-      <ul>
-        {data5.map((blog, index) => (
-            <li key = {index} >
-                <Link style = {{"backgroundColor" : "white", "color" : "black"}} href = {`Blog/${blog.title}`}>{blog.title}</Link>
-            </li>
-        ))}
-      </ul>
+      <BlogCards/>
     </>
   )
 }
