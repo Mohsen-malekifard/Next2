5dbd7facba6c2ea673a67a3d77fa189155430f59
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 07:40:02 +0000

    a

--- my-app/src/app/Blog/[id]/page.jsx
+++ my-app/src/app/Blog/[id]/page.jsx
@@ -1,8 +1,8 @@
 import data5 from "@/app/lib/data";
 import Link from "next/link";
 
-export default function getDetails(params) {
-    const found = data5.find((blog) => blog.id == params.id);
+export default function getDetails({params}) {
+    const found = data5.find((blog) => blog.title == params.id);
     return (
         <>
             <h1>{found.title}</h1>
