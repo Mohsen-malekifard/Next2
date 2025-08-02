0ec921ba282cc2ab25477d08c1cc88c7268b0512
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-02 14:50:45 +0000

    MN

--- my-app/src/components/BlogCards.jsx
+++ my-app/src/components/BlogCards.jsx
@@ -7,18 +7,24 @@ export default function BlogCards() {
 
 
     return (
-        <ul className="space-y-4">
+        <ul className="grid grid-cols-2 gap-4">
             {data5.map((blog, index) => (
-                <li key={index} className="rounded-lg shadow-lg overflow-hidden">
+                <li key={index} className="bg-white p-4 rounded-md shadow-md">
                 <Link 
-                    className="block px-4 py-2 text-lg font-semibold bg-white text-black hover:bg-gray-200 transition-colors duration-200 rounded-md"
+                    className="block text-lg font-semibold text-black mb-4"
                     href={`Blog/${blog.title}`}
                 >
                 {blog.title}
                 </Link>
+                <button 
+                    className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-200"
+                >
+                Read more
+                </button>
                 </li>
             ))}
         </ul>
+
 
     );
 }
